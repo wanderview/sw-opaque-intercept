3e5842949d93f76ee8d62c8479b2bce2f01c92ea
Ben Kelly <ben@wanderview.com>
2015-10-27 17:43:02 -0700

    Add links for different types of failures.

--- sw.js
+++ sw.js
@@ -9,8 +9,39 @@ self.addEventListener('active', function(evt) {
 self.addEventListener('fetch', function(evt) {
   console.log('fetch: ' + evt.request.url);
 
-  if (evt.request.url.endsWith('fake.html')) {
+  console.log('hmm ' + evt.request.url.endsWith('opaque'));
+
+  if (evt.request.url.endsWith('opaque')) {
     console.log('responding with example.com opaque response');
     evt.respondWith(fetch('https://example.com', { mode: 'no-cors' }));
+    return;
+  }
+
+  if (evt.request.url.endsWith('error')) {
+    console.log('responding with example.com error response');
+    evt.respondWith(Response.error());
+    return;
+  }
+
+  if (evt.request.url.endsWith('used')) {
+    console.log('responding with example.com used response');
+    var response = new Response('hello world');
+    response.text();
+    evt.respondWith(response);
+    return;
+  }
+
+  if (evt.request.url.endsWith('canceled')) {
+    evt.preventDefault();
+    return;
+  }
+
+  if (evt.request.url.endsWith('rejected')) {
+    evt.respondWith(Promise.reject('disaster!'));
+    return;
+  }
+
+  if (evt.request.url.endsWith('throws')) {
+    throw(new Error('boom'));
   }
 });
